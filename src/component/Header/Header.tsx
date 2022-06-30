ff1852ac97c9d17d0ffb13045537c39e05661345
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-06-30 19:46:56 +0400

    feat: add basic style header

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
+import './header.css';
+
 
 const Header = () => {
     return (
-        <header>
+        <header className="header">
             <div className="container">
-                <nav>
-                    <NavLink to='/'>Main</NavLink>
-                    <NavLink to='/work'>Work</NavLink>
+                <nav className="nav">
+                    <NavLink to='/' className="menu-item">Main</NavLink>
+                    <NavLink to='/work' className="menu-item">Work</NavLink>
                 </nav>
             </div>
         </header>
