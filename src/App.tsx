92199fb135b89bfbc6693d8055b43c82aaf7e9fa
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-06-30 14:12:49 +0400

    feat: add 2 pages main and work

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Header from './component/Header';
-
+import Header from "./component/Header";
+import Main from "./pages/Main";
+import Work from "./pages/Work";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Navigate to=""/>}/>
-          <Route />
+          <Route path="/" element={<Navigate to="/main" />} />
+          <Route path="/main" element={<Main />}/>
+          <Route path="/work" element={<Work />}/>
         </Routes>
       </div>
     </BrowserRouter>
