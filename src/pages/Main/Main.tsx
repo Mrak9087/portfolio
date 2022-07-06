92fa0f2276ca87b59139854f13c8a5285771a1b2
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-07-06 23:37:36 +0400

    feat: add media for main page

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,26 +1,34 @@
 import React from "react";
 import Container from "../../component/Container";
 
-import './main.css';
+import "./main.css";
 
 const Main = () => {
-    return (
-        <div className="main">
-            <div className="mainContainer">
-                <div className="mainInfo">
-                    <h3>Hello, my name is <span>Maxim</span></h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                        Ex rem animi aperiam laborum vel amet facilis perspiciatis ducimus? 
-                        Pariatur inventore eius ipsa eos voluptas, praesentium adipisci incidunt deserunt animi fugit?
-                    </p>
-                    <div className="mainImg">
-                        <img src="" alt="my photo" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="main">
+      <div className="mainContainer">
+        <div className="mainInfo">
+          <h3 className="hello">
+            Hello, my name is <span>Maxim</span>
+          </h3>
+          <h3 className="myProfession">
+            I`m a <span className="typing">Frontend Development</span>
+          </h3>
+          <p>
+            I have experience in programming in Delphi. I created clients to work with
+            databases: MS SQL, MySQL, Firebird. I know SQL. Basic knowledge of
+            Java. The institute had a course on HTMS, CSS. After graduation, I
+            was engaged in page layout for some time. I love solving various
+            puzzles. I want to become a professional developer. I am ready to
+            develop in this direction.
+          </p>
         </div>
-    )
-}
+        <div className="mainImg">
+          <img src="./photo.jpg" alt="my photo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
