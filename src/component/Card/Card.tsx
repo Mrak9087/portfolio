c02bc5179c83a3f4c61647d0eb69591fcf12e34f
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-07-11 21:25:18 +0400

    feat: add component Card

---
+++ src/component/Card/Card.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './card.css';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="cardImg">
+                <img src="./images/trello.png" alt="trello" />
+            </div>
+            <div className="cardInfo">
+                <h2 className="cardTitle">Trello Clone</h2>
+                <p className="cardDesc">
+                Система управления проектами – приложение помогающее достичь поставленные задачи 
+                отдельному человеку в команде или группе разработчиков.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Card from "../../component/Card";
 import SectionTitle from "../../component/SectionTitle";
+import "./work.css"
 
 const Work = () => {
     return (
         <div className="work">
             <SectionTitle text="Work" />
+            <div className="workContent">
+                <Card />
+            </div>
         </div>
     )
 }
