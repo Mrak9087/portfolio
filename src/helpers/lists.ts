6b1316092c20e277c50381243b27a28f21e4acbf
Mrak9087 <maks19-88@mail.ru>
2023-09-02 23:00:17 +0400

    feat: add new Work and new skills

--- src/helpers/lists.ts
+++ src/helpers/lists.ts
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 
 export const skillList = [
-  'HTML, CSS, SCSS, JavaScript, Typescript',
-  'React, Redux',
+  'HTML, CSS, SCSS, JavaScript, Typescript, Tailwind CSS',
+  'React, Redux, NextJS',
   'Git, GitHub',
   'REST API, Axios',
   'Visual Studio Code, WebStorm',
@@ -17,10 +17,6 @@ export const useEdList = () => {
     t('home.ed2'),
     t('home.ed3'),
     t('home.ed4')
-    // 'Balashovsky Institute of Saratov State University / Applied Informatics in Economics',
-    // 'Courses RS School / JS/FE PRE-SCHOOL',
-    // 'Courses RS School / JAVASCRIPT/FRONT-END 2021Q3',
-    // 'Courses RS School / REACT 2022 Q1',
   ];
   
 } 
@@ -34,12 +30,6 @@ export const useExpList = () => {
     t('home.exp4'),
     t('home.exp5'),
     t('home.exp6')
-    // '2008 - 2009 / Balashovsky Institute of Saratov State University / Laboratorian',
-    // '2010 - 2011 / Balashovsky Institute of Saratov State University / Engineer (Developer)',
-    // '2011 - 2015 / Social support / Leading Specialist (System administrator)',
-    // '2015 - 2018 / Urban Heating Networks Balashov / Specialist (System administrator)',
-    // '2018 - 2019 / Urban Heating Networks Balashov / Network administrator',
-    // '2020 - until now / MFC / Leading Specialist (System administrator)',
   ];
 }
 
